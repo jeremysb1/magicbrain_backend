abda9df456474f9b28ccbd09ddf370279273a1a3
Jeremy Hall <21281011+jeremysb1@users.noreply.github.com>
2021-01-06 14:17:22 -0500

    functionality to add users and update user data

--- face-recognition-brain-api/server.js
+++ face-recognition-brain-api/server.js
@@ -54,26 +54,34 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
+  let found = false;
   database.users.forEach(user => {
     if (user.id === id) {
-      res.json(user);
-    } else {
-      res.status(404).json('no such user');
-    }
+      found = true;
+      return res.json(user);
+    } 
   })
+  if (!found) {
+    res.status(400).json('not found');
+  }
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    } 
+  })
+  if (!found) {
+    res.status(400).json('not found');
+  }
 })
 
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 })
 
-
-/*  
-/--> res = this is working
-/signin --> POST = success || fail
-/register --> POST = user
-/profile/:userID --> GET = user
-/image --> PUT --> user
-
-
-*/
